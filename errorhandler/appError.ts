987ee2f4d864e85363e0056d3963b82dc701a2e3
bettytadege <bettytadege13@gmail.com>
2024-11-24 04:10:57 +0300

    feat(error-handler): add custom error handler

---
+++ errorhandler/appError.ts
@@ -0,0 +1,18 @@
+class AppError extends Error {
+    //define their type 
+    public statusCode:number
+    public status:string
+    public isOperational:Boolean
+
+    constructor(message:string,statusCode:number) {
+        console.log('message:', message);
+        console.log('statusCode:', statusCode);
+        super(message); 
+        this.statusCode = statusCode; 
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true; 
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default AppError;
