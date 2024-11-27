7ad67698c4541d6a4607962b4ec5b989c9b0aaa7
bettytadege <bettytadege13@gmail.com>
2024-11-27 08:52:25 +0300

    feat(jwt):impelment jwt authentication

---
+++ utils/auth.ts
@@ -0,0 +1,12 @@
+import jwt, { Secret } from 'jsonwebtoken'
+
+export const signToken = (payload:{id:number,role:string})=> {
+  const {
+    id,
+    role,
+  }=payload
+  
+  const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as Secret;
+  const EXPIRES_IN = process.env.EXPIRES_IN;
+  return jwt.sign(payload ,JWT_SECRET_KEY, { expiresIn: EXPIRES_IN });
+};
