599d88036537532c8784f71fdaf8693da74bf8dd
bettytadege <bettytadege13@gmail.com>
2024-11-27 21:55:41 +0300

    feat(route):add user route

---
+++ routes/userRoute.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import {getAllUser, getOneUser, login, register} from '../controller/userController'
+const router=express.Router()
+
+router.route('/').get(getAllUser)
+router.route('/:id').get(getOneUser)
+router.post('/register',register)
+router.post('/login',login)
+
+export default router
