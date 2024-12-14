4610147a1fb0d622c771a05afac54ded2fa3fca6
bettytadege <bettytadege13@gmail.com>
2024-12-14 16:00:24 +0300

    refactor(provider controller):remove logging statements and change getoneProvider to getProviderProfile

--- controller/parkingProviderController.ts
+++ controller/parkingProviderController.ts
@@ -8,7 +8,6 @@ import{v4 as uuidv4 } from "uuid"
 // REGISTER PARKING PROVIDER
 export const register = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log('provider')
     const providerId=uuidv4()
     //get email and admin id from request body
     const { email, adminId } = req.body;
@@ -20,7 +19,6 @@ export const register = catchAsync(
     if (!admin) {
       return next(new AppError("No admin account found", 404));
     }
-    // console.log('admin',admin)
     // Check if provider already exists
     const provider = await prisma.parkingProvider.findUnique({
       where: { email }, 
@@ -41,13 +39,13 @@ export const register = catchAsync(
     });
     // console.log('new provider:',newProvider)
     // Generate token
-    const token = signToken({ id: newProvider.id });
+    // const token = signToken({ id: newProvider.id });
     
     // Send response
     res.status(201).json({
       status: "success",
       message: "Provider registered successfully",
-      token,
+      // token,
       newProvider,
     });
   }
@@ -57,9 +55,11 @@ export const register = catchAsync(
 
 export const getAllProvider = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    // filtering, pagnation
     const page = parseInt(req.query.page as string) || 10
     const limit = parseInt( req.query.limit as string ) || 20
     const skip:number = (page - 1)* limit
+    
     // find all provider
     const provider = await prisma.parkingProvider.findMany(
       {
@@ -78,7 +78,7 @@ export const getAllProvider = catchAsync(
   }
 );
 
-export const getOneProvider = catchAsync(
+export const getProviderProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
 
     const { id } = req.params;
@@ -104,21 +104,22 @@ export const getOneProvider = catchAsync(
 export const updateProvider = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
-    //
+    //find provider using provider id
       const provider = await prisma.parkingProvider.findUnique({
         where: { id},
       });
-      console.log('provider',provider)
+      // if provider is not found
       if (!provider) {
         return next(new AppError("no provider found with this id", 400));
       }
-    //
+     //update provider
      const updatedProvider= await prisma.parkingProvider.update({ where:{ id},data:{
         ...req.body,
         adminId: req.body.adminId , 
         totalSpace: req.body.totalSpace ? parseInt(req.body.totalSpace) : undefined,
         availableSpace: req.body.availableSpace ? parseInt(req.body.availableSpace) : undefined,
      } });
+     //send reponse
       res.status(200).json({
         status: "success",
         message: "provider updated sucessfully",
