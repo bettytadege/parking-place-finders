967df1c74fa2cf3d329ee407f7aebab4196b1dd7
bettytadege <bettytadege13@gmail.com>
2024-12-14 16:18:31 +0300

    refactor(jwt sugntoken): remove unecessary import

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,6 +1,7 @@
 import jwt, { Secret } from 'jsonwebtoken'
+import { S } from 'vite/dist/node/types.d-aGj9QkWt';
 
-export const signToken = (payload:{id:number,role:string})=> {
+export const signToken = (payload:{id:string,role?:string})=> {
   const {
     id,
     role,
