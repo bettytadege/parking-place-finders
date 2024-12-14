bc0d810d71e6ed5d2376fb7230180f9db5cd73a0
bettytadege <bettytadege13@gmail.com>
2024-12-14 16:04:29 +0300

    feat(firebase): add firebase config

---
+++ firebase/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID, // Note the corrected spelling
+};
+
+// Initialize Firebase App
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
