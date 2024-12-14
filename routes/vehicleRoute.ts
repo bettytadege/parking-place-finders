882821c9528c41c839403cf036e13a0f29e8ce06
bettytadege <bettytadege13@gmail.com>
2024-12-14 16:05:30 +0300

    refactor(vehicle route):remove commented route

--- routes/vehicleRoute.ts
+++ routes/vehicleRoute.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { deleteVehicle, register, updateVehicle} from '../controller/vehicleController'
 const router=express.Router()
 
-// router.route('/').get(getAllUser)
+
 router.route('/:id').delete(deleteVehicle).patch(updateVehicle)
 router.post('/register',register)
 
