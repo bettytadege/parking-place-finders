67d3e7f548e73e0c76871d262318e8d4622ef073
bettytadege <bettytadege13@gmail.com>
2024-12-15 20:54:37 +0300

    refactor(user controller):add firebase authentication

--- controller/userController.ts
+++ controller/userController.ts
@@ -5,41 +5,54 @@ import AppError from "../errorhandler/appError";
 import { signToken } from "../utils/auth";
 import bcrypt, { compare } from "bcryptjs";
 import { v4 as uuidv4 } from 'uuid';
+import admin  from "../firebase/admin";
 
 
 //REGISTER USER
 export const register = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password} = req.body;
+    const { email, password,role} = req.body;
+    if(!email || !password){
+      return next(new AppError("please provide email and password", 400));
+    }
     const userId=uuidv4()
-    console.log('id:',userId,typeof userId)
+    // console.log('id:',userId,typeof userId)
     const existingUser = await prisma.user.findUnique({
       where: {
         email,
       },
     });
-    console.log(existingUser)
-    // if user exist send error message
+    console.log('existing user ',existingUser)
+    // if user exist send  message
     if (existingUser) {
       return next(new AppError("User already exists with this email", 400));
     }
-    //hashing password
+    // hashing password
     const hashPassword = await bcrypt.hash(password, 12);
+    console.log(hashPassword)
     //create user if not exist
+    const userRecord=await admin.auth().createUser({
+      email,
+      password,
+    
+    })
+  console.log('user record:',userRecord)
+    // console.log('token:',token)
     const newUser = await prisma.user.create({
       data:
-      { ...req.body,
+      { 
+        ...req.body,
+        firebaseUid:userRecord.uid,
         id:userId,
-      password:hashPassword,}
+      password:hashPassword,
+    }
     });
-    // console.log("newuse::",newUser)
-    //generate token
-    const token = signToken({ id: newUser.id, role: newUser.role });
+    console.log("newuser::",newUser)
+   
     // send response
     res.status(201).json({
       status: "success",
       message: "user registered succssfully",
-      token,
       newUser,
     });
   }
