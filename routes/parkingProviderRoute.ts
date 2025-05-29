885ae8233c25a79880079ae7f6a6162330164d21
bettytadege <bettytadege13@gmail.com>
2025-05-29 15:44:19 +0300

    refactor:rename routes

--- routes/parkingProviderRoute.ts
+++ routes/parkingProviderRoute.ts
@@ -1,8 +1,8 @@
 import express from 'express'
-import { getAllProvider, getProviderProfile, register, updateProvider} from '../controller/parkingProviderController'
+import { getAllProvider, getProviderProfile, register, Search, updateProvider} from '../controller/parkingProviderController'
 const router=express.Router()
 
-router.route('/').get(getAllProvider)
+router.route('/').get(getAllProvider).get(Search)
 router.route('/:id').get(getProviderProfile).patch(updateProvider)
 router.post('/register',register)
 
